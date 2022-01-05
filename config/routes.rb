5f8bdf2619e4848dff96a46fb0882e9a3739f589
medaminedev66 <md.amine.smahi@gmail.com>
2022-01-05 09:20:19 +0100

    Generate groups controller and its views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'users#index'
   devise_for :users
+  resources :groups, only: [:new, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
